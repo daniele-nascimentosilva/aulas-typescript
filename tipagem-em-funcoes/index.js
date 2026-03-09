bdf6000a1f1aa5f94e1013da0da01ccc9dd9a0ef
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 13:18:45 -0300

    adicionado arquivos da aula tipagem-em-funcoes

---
+++ tipagem-em-funcoes/index.js
@@ -0,0 +1,9 @@
+// function greet(name: string): void {
+//     console.log(`Olá, ${name}!`);
+// }
+// greet("Daniele");
+function doubleNumber(number) {
+    return number * 2;
+}
+var result = doubleNumber(5);
+console.log("O dobro de 5 \u00E9 ".concat(result, "."));
---
+++ tipagem-em-funcoes/index.ts
@@ -0,0 +1,20 @@
+// function greet(name: string): void {
+//     console.log(`Olá, ${name}!`);
+// }
+
+// greet("Daniele");
+
+function doubleNumber(number: number): number {
+    return number * 2;
+}
+
+const result = doubleNumber(5);
+console.log(`O dobro de 5 é ${result}.`);
+
+
+//exercício
+function soma(a: number, b: number): number {
+    return a + b;
+}
+const resultadoSoma = soma(3, 7);
+console.log(`A soma de 3 e 7 é ${resultadoSoma}.`);
